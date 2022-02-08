128393452812e83cf1fd54a2faac9fb888e6a8ba
Alexey Dinaychuk <alexey.dinaychuk@artpix3d.com>
2022-02-09 00:53:39 +0200

    - add checking codec name && constants remove double os.exit

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ import (
 	"os"
 )
 
+const CodecH264 string = "h264"
+const CodecH265 string = "h265"
+
+var supportedCodecs = [2]string{CodecH264, CodecH265}
+
 type Stream struct {
 	url     string
 	camName string
@@ -21,6 +26,12 @@ type Context struct {
 	AVFormatCtx    *C.AVFormatContext
 	AVStream       *C.AVStream
 	AVCodecContext *C.AVCodecContext
+	AVPacket       Packet
+}
+
+type Packet struct {
+	AVPacket *C.AVPacket
+	AVFrame  *C.AVFrame
 }
 
 func main() {
@@ -52,22 +63,30 @@ func (s *Stream) Screen() {
 }
 
 func (context *Context) Read() {
-	fmt.Println("Start on go lang encode")
 
-	avFrame := C.av_frame_alloc()
-	if avFrame == nil {
+	if !context.canWatch() {
+		log.Fatal("Video codec of stream not support")
+	}
+
+	if context.needTranscode() {
+		fmt.Println("Need transcode:\n" + CodecH265 + " codec detected")
+	}
+
+	context.AVPacket.AVFrame = C.av_frame_alloc()
+	if context.AVPacket.AVFrame == nil {
 		log.Fatal("Error alloc frame data")
-		os.Exit(0)
 	}
-	avPacket := C.av_packet_alloc()
-	if avPacket == nil {
+	context.AVPacket.AVPacket = C.av_packet_alloc()
+	if context.AVPacket.AVPacket == nil {
 		log.Fatal("Error alloc packet data")
-		os.Exit(0)
 	}
+
 	var response C.int = 0
-	for i := 5; C.av_read_frame(context.AVFormatCtx, avPacket) >= 0 && i > 0; {
-		if avPacket.stream_index == 0 {
-			response = C.decode_packet(avPacket, context.AVCodecContext, avFrame)
+	for i := 5; C.av_read_frame(context.AVFormatCtx, context.AVPacket.AVPacket) >= 0 && i > 0; {
+
+		// check this is video stream (0) TODO refactor for true video check stream (not always 0 stream)
+		if context.AVPacket.AVPacket.stream_index == 0 {
+			response = C.decode_packet(context.AVPacket.AVPacket, context.AVCodecContext, context.AVPacket.AVFrame)
 			if C.has_decode_error(response) {
 				continue
 			} else if response < 0 {
@@ -75,8 +94,25 @@ func (context *Context) Read() {
 			}
 			i--
 		}
-		C.av_packet_unref(avPacket)
+		C.av_packet_unref(context.AVPacket.AVPacket)
 	}
-	C.av_frame_free(&avFrame)
-	C.av_packet_free(&avPacket)
+	C.av_frame_free(&context.AVPacket.AVFrame)
+	C.av_packet_free(&context.AVPacket.AVPacket)
+}
+
+func (context *Context) decodePacket() {
+
+}
+
+func (context *Context) canWatch() bool {
+	for _, codec := range supportedCodecs {
+		if codec == C.GoString(context.AVCodecContext.codec.name) {
+			return true
+		}
+	}
+	return false
+}
+
+func (context *Context) needTranscode() bool {
+	return CodecH264 == C.GoString(context.AVCodecContext.codec.name)
 }
